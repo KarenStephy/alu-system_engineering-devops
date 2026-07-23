3ece8178305bc0a609f3c073a80bcafb3ff16abf
KarenStephy <k.musangwa@alustudent.com>
2026-07-23 11:32:45 +0000

    successful

---
+++ ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+ne { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  match  => '^\s*PasswordAuthentication\s+(yes|no)',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+  match  => '^\s*IdentityFile\s+.*',
+}
